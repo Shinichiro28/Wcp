7857dde4e341739d9462bd5e868e2174fef5609e
Shinichiro28 <102603513+Shinichiro28@users.noreply.github.com>
2022-08-11 17:50:05 +0900

    First Commit

---
+++ src/Chapter01.java
@@ -0,0 +1,19 @@
+
+public class Chapter01 {
+	public static void main(String[] args) {
+		int a = 3;
+		int b = 5;
+		System.out.println("縦幅" + a + "横幅" + b + "の長方形の面積は、" + a * b);
+
+		boolean result = true;
+		char str = '駆';
+		double pi = 3.14;
+		long number = 314159265853979L;
+		String mogi = "ミナトの攻撃!　敵に15ポイントのダメージを与えた。";
+		System.out.println(result);
+		System.out.println(str);
+		System.out.println(pi);
+		System.out.println(number);
+		System.out.println(mogi);
+	}
+}
