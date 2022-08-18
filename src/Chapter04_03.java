37b19924b21356c173af4b4a8fab9f1d7ea936fa
Shinichiro28 <102603513+Shinichiro28@users.noreply.github.com>
2022-08-18 18:58:53 +0900

    [add] Chapter04 second commit

---
+++ src/Chapter04_03.java
@@ -0,0 +1,14 @@
+
+public class Chapter04_03 {
+	public static void main(String[] args) {
+		System.out.println("メソッドを呼び出します");
+		hello("湊さん");
+		hello("朝香さん");
+		hello("菅原さん");
+		System.out.println("メソッドの呼び出しが終わりました");
+	}
+
+	public static void hello(String name) {
+		System.out.println(name + "、こんにちは");
+	}
+}
---
+++ src/Chapter04_04.java
@@ -0,0 +1,12 @@
+
+public class Chapter04_04 {
+	public static void main(String[] args) {
+		add(100, 20);
+		add(30, 60);
+	}
+
+	public static void add(int x, int y) {
+		int ans = x + y;
+		System.out.println(x + "+" + y + "=" + ans);
+	}
+}
